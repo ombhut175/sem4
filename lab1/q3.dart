217a26d30418af5143dd4471f0562f8f497c8138
om <codersarea12@gmail.com>
2024-12-15 13:24:11 +0530

    new files deleted

--- lab1/q3.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main(){
-    stdout.writeln("Enter F");
-    double f = double.parse(stdin.readLineSync()!);
-    double c = (f-32)*(5/9);
-
-    stdout.writeln("f = $f c = $c");
-}
--- lab1/q5.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(){
-    stdout.writeln("enter meter");
-    int meter = int.parse(stdin.readLineSync()!);
-    double feet = 3.28084 * meter;
-    stdout.writeln(feet);
-}
